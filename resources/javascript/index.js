cb1fe66d77b95249eeb84217fdea594d9c51a347
haohsin <haohsin0303@gmail.com>
2019-09-28 21:01:58 +0800

    add a jquery file

--- resources/javascript/index.js
+++ resources/javascript/index.js
@@ -1,15 +1,10 @@
-$(function(){
+"use strict";
 
-    // 偵聽a click
-    $(".menu").bind("click",function(){
- 
-        var section_name = $(this).attr("section_name");
-        var target_top = $("#"+section_name).offset().top;
-        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
-
-        $body.animate({
-            scrollTop: target_top
-        }, 800);
- 
-    })
- })
+var anchor = document.querySelector('a[href="#dest"]')
+var target = document.getElementById('dest')
+anchor.addEventListener('click', function (e) {
+  if (window.scrollTo) {
+    e.preventDefault()
+    window.scrollTo({'behavior': 'smooth', 'top': target.offsetTop})
+  }
+})
